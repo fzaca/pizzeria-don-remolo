e1274e1e1618a3b77aad23483b748fc75d475a74
Xukay101 <zaca03zaca@gmail.com>
2023-05-21 00:57:00 -0300

    Update commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from blueprints import menu
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+app.register_blueprint(menu)
+
+@app.route('/test')
+def test():
     return 'Hola mundo'
 
 if __name__ == '__main__':
