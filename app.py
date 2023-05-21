78a40dd465ccba4aa1413807e089f373ea08be86
Xukay101 <zaca03zaca@gmail.com>
2023-05-21 20:02:24 -0300

    Update commit

--- app.py
+++ app.py
@@ -1,13 +1,59 @@
-from flask import Flask
-from blueprints import menu
+import os
+
+from flask import Flask, render_template, session, request, redirect, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-app.register_blueprint(menu)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return 'Hola mundo'
+@app.route('/cart')
+def cart():
+    if 'cart' not in session or len(session['cart']) == 0:
+        cart = []
+    cart = session['cart']
+    total = sum([_ for product in cart])
+    return render_template('cart.html', cart=cart)
 
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    name = request.form['name']
+    price = float(request.form['price'])
+    quantity = int(request.form['quantity'])
+    title = request.form['title']
+
+    if 'cart' not in session:
+        session['cart'] = []
+
+    for product in session['cart']:
+        if product['name'] == name:
+            product['quantity'] += quantity
+            product['price'] = price * product['quantity']
+
+            flash("El producto se ha actualizado correctamente.")
+            return redirect('/cart')
+
+    product = {
+        'name': name, 
+        'title': title,
+        'price': price * quantity,
+        'quantity': quantity,
+        'image_path': url_for('static', filename=f'images/{name.lower()}.jpg'),
+    }
+
+    session['cart'].append(product)
+
+    flash("El producto se ha agregado al carrito correctamente.")
+    return redirect('/cart')
+
+@app.route('/delete_to_cart', methods=['POST'])
+def delete_to_cart():
+    pos = request.form['pos']
+    session['cart'].pop(int(pos))
+    flash("El producto se ha eliminado del carrito correctamente.")
+    return redirect('/cart')
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
--- blueprints/menu.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-menu = Blueprint('order', __name__)
-
-@menu.route('/')
-def index():
-    return render_template('index.html')
